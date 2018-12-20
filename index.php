effcfcd113d3785eb39de805092c06a789602828
RonAk47 <rjronakjain9@gmail.com>
2018-12-20 20:04:57 +0530

    Improved AutoComplete Suggestion

--- index.php
+++ index.php
@@ -6,7 +6,12 @@
 		<link href="assets/css/truebus.css" rel="stylesheet">
       	<link href="assets/css/parsley.css" rel="stylesheet">
       	<link href="assets/css/datepick.css" rel="stylesheet">
-         <link href="assets/css/jquery-datepicker" rel="stylesheet">
+        <link href="assets/css/jquery-datepicker" rel="stylesheet">
+      	<script src="assets/js/jquery.js"></script> 
+      	<script src="http://code.jquery.com/ui/1.10.2/jquery-ui.js" ></script>
+   		<script src="assets/js/jquery.raty.js"></script>
+   		
+   		<link href="assets/css/autoComplete.css" rel="stylesheet">
      </head>
 <body>
 <div class="tb_header">
@@ -39,19 +44,19 @@
 <div class="container" ng-controller="search">
             <div class="row" style="min-height:400px;margin-top:120px;">
                <div class="col-md-6">
-            <form id="myForm" method="post" data-parsley-validate="" >
+            <form id="myForm" method="post" data-parsley-validate="" autocomplete="off">
                   <section id="Search" class="LB XXCN  P20">
                      <h1 class="bookTic XCN TextSemiBold" >Online Bus Tickets Booking with Zero Booking Fees</h1>
                      <div class="searchRow clearfix">
                         <div class="LB">
                            <label class="inputLabel">From</label>
-                           <input id="board_point"  class="XXinput searching" placeholder="Enter a city" type="text"  data-id="board_point" autocomplete="off" data-parsley-error-message="Please select a source city" tabindex="1" required/>
+                           <input id="myInput"  class=" XXinput searching" placeholder="Enter a city" type="text"  data-id="board_point"  data-parsley-error-message="Please select a source city" name="myCountry" tabindex="1" required/>
                            <div class="errorMessageFixed"> </div>
                         </div>
                         <span class="switchButton" id="switchButton"></span>
-                        <div class="searchRight NoPaddingRight">
+                        <div class="searchRight NoPaddingRight ">
                            <label class="inputLabel">To</label>
-                           <input id="Destination" class="XXinput searching" placeholder="Enter a city" type="text" tabindex="2" data-id="drop_point"  autocomplete="off" data-parsley-error-message="Please select a destination city" required />
+                           <input id="myInp" class="XXinput searching " placeholder="Enter a city" type="text" tabindex="2" data-id="drop_point"   data-parsley-error-message="Please select a destination city" name="myCountry" required />
                            <div class="errorMessageFixed"> </div>
                         </div>
                      </div>
@@ -59,17 +64,18 @@
                         <div class="LB">
                            <label class="inputLabel">Date of Journey</label>
                            <span class="blackTextSmall"></span>
-                           <input id="Calenderfrom " class="XXinput calendar date-pick  pickup_date pickup_datef Calenderfrom" placeholder="dd-mm-yyyy" readonly type="text" title="Date in the format dd-mmm-yyyy"  tabindex="3" />
+                           <input  class="XXinput  date-pick  pickup_date pickup_datef " placeholder="dd-mm-yyyy"  type="date" title="Date in the format dd-mmm-yyyy" tabindex="3" />
                         </div>
                         <div class="searchRight retJouney">
                            <label class="inputLabel">Date of Return<span class="opt">&nbsp;(Optional)</span></label>
-                           <input id="Calenderreturn" class="XXinput calendar date-pick pickup_dater" placeholder="dd-mm-yyyy" type="text" title="Date in the format dd-mmm-yyyy" readonly  tabindex="4" />
+                           <input class="XXinput  date-pick  pickup_date pickup_datef" placeholder="dd-mm-yyyy" type="date" title="Date in the format dd-mmm-yyyy" tabindex="4" />
                         </div>
                      </div>
                      <div class="dateError">Onward date should be before return date</div>
                      <button class="RB Xbutton" id="searchBtn" ng-click="homesearch()">Search Buses</button>
                   </section>
               </form>
+              <script src="assets/js/autoComplete.js"></script>
                </div>
                <div class="col-md-6">
                   <div class="tb_bus">
